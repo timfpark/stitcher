504396377515dfb6924f3f8d7bda575cc2b422db
Tim Park <timfpark@gmail.com>
2013-06-22 07:50:08 -0700

    use blob.get again now that its fixed

--- stitcher.js
+++ stitcher.js
@@ -4,7 +4,7 @@ var async = require('async')
   , fs = require('fs')
   , nitrogen = require('nitrogen');
 
-var user = new nitrogen.User({ local_id: 'user', email: "timfpark@gmail.com", password: "[euro4sure]" });
+var user = new nitrogen.User({ nickname: 'user', email: "timfpark@gmail.com", password: "xxxxxxxx" });
 
 var service = new nitrogen.Service(config);
 service.authenticate(user, function(err, session, user) {
@@ -12,29 +12,34 @@ service.authenticate(user, function(err, session, user) {
     if (err) return console.log('failed to connect user: ' + err);
 
     // search for all sunset images from camera, sorted in oldest to newest
-    nitrogen.Message.find(session, { from: "51b11460446230d078000007", type: 'image' }, /*{ ts: 1 },*/ function(err, images) {
+    nitrogen.Message.find(session, { from: "51c4ba82292b19f00b0013d4", type: 'image' }, {}, function(err, images) {
         if (err) return console.log('finding images to stitch failed: ' + err);
 
         async.eachLimit(images, 50, function(image, callback) {
+
+
             nitrogen.Blob.get(session, image.body.url, function(err, resp, blob) {
                 if (err) return console.log('downloading image ' + image.id + ' failed.');
-                var filename = 'images/' + image.ts.getTime() + '.png';
+
+                var filename = 'images/' + image.ts.getTime() + '.jpg';
                 fs.writeFile(filename, blob, 'binary', function(err) {
                     if (err) return console.log('saving image failed');
 
                     console.log(filename + ' downloaded.');
                     callback(err);
-                })
+                });
 
             });
+
         }, function (err) {
             if (err) return console.log('failed to download all images, bailing.');
 
             console.log('images downloaded successfully.');
-            //var createVideoCommand = "ffmpeg -f image2 -r 30 -pattern_type glob -i 'images/*.png' -c:v libx264 out.mp4";
-            //exec(createVideoCommand, function (err, stdout, stderr) {
-            //    if (err) return console.log('failed to build video.');
-            //});
+            var createVideoCommand = "ffmpeg -f image2 -r 30 -pattern_type glob -i 'images/*.jpg' -c:v libx264 timelapse.mp4";
+            exec(createVideoCommand, function (err, stdout, stderr) {
+                if (err) return console.log('failed to build video.');
+                console.log('created video');
+            });
 
         });
     });
