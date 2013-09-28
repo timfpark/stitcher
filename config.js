fd7dc13c49e5802dfe8b7af0ba117e7722b92885
Tim Park <timfpark@gmail.com>
2013-09-27 20:10:59 -0700

    use LevelDB store

--- config.js
+++ config.js
@@ -1,15 +1,10 @@
 var nitrogen = require('nitrogen');
 
 var config = {
-        host: 'api.nitrogen.io',
-        http_port: 443,
-        protocol: 'https'
+//    host: 'api.nitrogen.io',
+//    protocol: 'https'
 };
 
-config.local_store_path = ".";
-config.store = new nitrogen.FileStore(config);
-
-config.base_url = config.protocol + "://" + config.host + ":" + config.http_port + "/api/v1";
-config.realtime_url = config.base_url + "/realtime";
+config.store = new nitrogen.Store(config);
 
 module.exports = config;
