716efad09758a6910e269c8ade8fb89123c55053
Tim Park <timfpark@gmail.com>
2014-04-04 16:27:07 -0700

    bring stitcher up to snuff.

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
-var nitrogen = require('nitrogen');
+var nitrogen = require('nitrogen')
+  , Store = require('nitrogen-leveldb-store');
 
 var config = {
 //    host: 'api.nitrogen.io',
 //    protocol: 'https'
 };
 
-config.store = new nitrogen.Store(config);
+config.store = new Store(config);
 
 module.exports = config;
--- stitcher.js
+++ stitcher.js
@@ -4,7 +4,11 @@ var async = require('async')
   , fs = require('fs')
   , nitrogen = require('nitrogen');
 
-var user = new nitrogen.User({ nickname: 'user', email: "timfpark@gmail.com", password: "xxxxxxxx" });
+var user = new nitrogen.User({
+    nickname: 'user',
+    email: process.env.NITROGEN_EMAIL,
+    password: process.env.NITROGEN_PASSWORD
+});
 
 var service = new nitrogen.Service(config);
 service.authenticate(user, function(err, session, user) {
@@ -12,35 +16,36 @@ service.authenticate(user, function(err, session, user) {
     if (err) return console.log('failed to connect user: ' + err);
 
     // search for all sunset images from camera, sorted in oldest to newest
-    nitrogen.Message.find(session, { from: "51c4ba82292b19f00b0013d4", type: 'image' }, {}, function(err, images) {
+    nitrogen.Message.find(session, { from: "52c33da4b77d9aac07000025", type: 'image', tags: 'sunset-1' }, {}, function(err, images) {
         if (err) return console.log('finding images to stitch failed: ' + err);
 
-        async.eachLimit(images, 50, function(image, callback) {
+        async.eachLimit(images, 10, function(image, callback) {
+            var filename = 'images/' + image.ts.getTime() + '.jpg';
 
+            if (!fs.existsSync(filename)) {
+                nitrogen.Blob.get(session, image.body.url, function(err, resp, blob) {
+                    if (err) return callback(err);
 
-            nitrogen.Blob.get(session, image.body.url, function(err, resp, blob) {
-                if (err) return console.log('downloading image ' + image.id + ' failed.');
+                    fs.writeFile(filename, blob, 'binary', function(err) {
+                        if (err) return console.log('saving image failed');
 
-                var filename = 'images/' + image.ts.getTime() + '.jpg';
-                fs.writeFile(filename, blob, 'binary', function(err) {
-                    if (err) return console.log('saving image failed');
-
-                    console.log(filename + ' downloaded.');
-                    callback(err);
+                        console.log(filename + ' downloaded.');
+                        callback();
+                    });
                 });
-
-            });
-
+            } else {
+                console.log(filename + ' already downloaded.');
+                callback();
+            }
         }, function (err) {
             if (err) return console.log('failed to download all images, bailing.');
 
             console.log('images downloaded successfully.');
-            var createVideoCommand = "ffmpeg -f image2 -r 30 -pattern_type glob -i 'images/*.jpg' -c:v libx264 timelapse.mp4";
+            var createVideoCommand = "ffmpeg -f image2 -r 20 -pattern_type glob -i 'images/*.jpg' -c:v libx264 timelapse.mp4";
             exec(createVideoCommand, function (err, stdout, stderr) {
                 if (err) return console.log('failed to build video.');
                 console.log('created video');
             });
-
         });
     });
 
